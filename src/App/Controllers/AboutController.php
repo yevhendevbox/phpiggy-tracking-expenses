fd1f9d72fe135ea29879c429902273187f2bde22
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-02-15 16:11:48 +0200

    feat: added templates for home, about page

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -7,7 +7,7 @@ namespace App\Controllers;
 use Framework\TemplateEngine;
 use App\Config\Paths;
 
-class HomeController
+class AboutController
 {
   private TemplateEngine $view;
 
@@ -16,10 +16,10 @@ class HomeController
     $this->view = new TemplateEngine(Paths::VIEW);
   }
 
-  public function home()
+  public function about()
   {
-    echo $this->view->render('/index.php', [
-      'title' => 'Home Page',
+    echo $this->view->render('/about.php', [
+      'title' => 'About Page',
     ]);
   }
 }
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,8 +6,10 @@ include __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 use App\Controllers\HomeController;
+use App\Controllers\AboutController;
 
 $app = new App();
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
