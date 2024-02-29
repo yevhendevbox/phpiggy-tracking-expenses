01a929fa173cd202c27d29ec17aba9fc24251d4a
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-02-29 10:41:21 +0200

    ref: updated gitignore

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,11 +2,21 @@
 
 declare(strict_types=1);
 
-use Framework\TemplateEngine;
+use Framework\{TemplateEngine, Database};
 use App\Config\Paths;
 use App\Services\ValidatorService;
 
 return [
   TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
-  ValidatorService::class => fn () => new ValidatorService()
+  ValidatorService::class => fn () => new ValidatorService(),
+  Database::class => fn () => new Database(
+    'mysql',
+    [
+      'host' => '127.0.0.1',
+      'port' => '3308',
+      'dbname' => 'phpiggy'
+    ],
+    'root',
+    ''
+  )
 ];
