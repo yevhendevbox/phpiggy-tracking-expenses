c4b22d7ab47ea68dfd2a2285c0e8b7fbc6c5cf77
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-03-01 10:08:44 +0200

    feat: added user service file

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 
 class AuthController
 {
   public function __construct(
     private TemplateEngine $view,
-    private ValidatorService $validatorService
+    private ValidatorService $validatorService,
+    private UserService $userService
   ) {
   }
 
@@ -24,6 +25,5 @@ class AuthController
   public function register()
   {
     $this->validatorService->validateRegister($_POST);
-    // dd($_POST);
   }
 }
