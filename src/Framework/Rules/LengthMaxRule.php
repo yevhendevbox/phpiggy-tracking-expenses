93a115ddafe83bdd6d31fd93602cc5d9bde7fe72
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-03-15 17:28:47 +0200

    feat: validating transaction form

---
+++ src/Framework/Rules/LengthMaxRule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework\Rules;
+
+use Framework\Contracts\RuleInterface;
+use InvalidArgumentException;
+
+class LengthMaxRule implements RuleInterface
+{
+  public function validate(array $data, string $field, array $params): bool
+  {
+    if (empty($params[0])) {
+      throw new InvalidArgumentException("Max length not specified!");
+    }
+    (int) $length = $params[0];
+    return strlen($data[$field]) < $length;
+  }
+
+  public function getMessage(array $data, string $field, array $params): string
+  {
+    return "Exceeds maximum character limit of {$params[0]} characters!";
+  }
+}
