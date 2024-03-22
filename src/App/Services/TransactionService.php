7b22055159b4e478a2c3f49021fb6b6ac183c84e
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-03-22 16:53:19 +0200

    feat: search transactions form, paggination added

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -28,4 +28,32 @@ class TransactionService
       ]
     );
   }
+
+  public function getUserTransactions(int $length, int $offset)
+  {
+    $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
+    $params =       [
+      'user_id' => $_SESSION['user'],
+      'description' => "%{$searchTerm}%"
+    ];
+
+    $transactions = $this->db->query(
+      "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') AS formatted_date
+      FROM transactions WHERE user_id = :user_id
+      AND description LIKE :description
+      LIMIT {$length} OFFSET {$offset}
+      ",
+      $params
+    )->findAll();
+
+    $transactionCount = $this->db->query(
+      "SELECT COUNT(*)
+      FROM transactions WHERE user_id = :user_id
+      AND description LIKE :description
+      ",
+      $params
+    )->count();
+
+    return [$transactions, $transactionCount];
+  }
 }
